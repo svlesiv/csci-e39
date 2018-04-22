b7c16b034ac5d6c2c6ce355e18af755d74e17f97
Svitlana Lesiv <lesivsvitlana@gmail.com>
2018-04-22 11:32:46 -0400

    bold-text

--- src/assignments/project-2/listItem.jsx.js
+++ src/assignments/project-2/listItem.jsx.js
@@ -2,18 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class ListItem extends React.Component {
-  render() {
-    const listData = this.props;
+ render() {
+   const listData = this.props;
+   let maskData = listData.text ? listData.text : '';
 
-    if(listData.type === 'member') {
-      return <li key={listData.key}>Name: <span>{listData.name}</span></li>
-    } else if(listData.type === 'message') {
-      return <li key={listData.key}>
-            <label>{listData.name} at {listData.postedAt}</label>
-            <p>{listData.text}</p>
-          </li>
-    }
-  };
+   let isBold = maskData.match(/^\*.*\*$/);
+
+   if(listData.type === 'member') {
+     return <li key={listData.key}>Name: <span>{listData.name}</span></li>
+   } else if(listData.type === 'message') {
+     return <li key={listData.key}>
+           <label>{listData.name} at {listData.postedAt}</label>
+         {isBold ? (
+           <p className="bold-text">{listData.text}</p>
+         ) : (
+           <p>{listData.text}</p>
+         )}
+         </li>
+   }
+ };
 }
 
 export default ListItem
